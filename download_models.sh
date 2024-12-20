6fcbb35fe56c241fcbfd908741b9cc69ef26cb47
ml-inory <584688538@qq.com>
2024-12-20 16:21:43 +0800

    Delete unused models

---
+++ download_models.sh
@@ -0,0 +1,7 @@
+cd models
+wget https://github.com/ml-inory/melotts.axera/releases/download/v1.0/encoder-zh.onnx
+wget https://github.com/ml-inory/melotts.axera/releases/download/v1.0/encoder-jp.onnx
+wget https://github.com/ml-inory/melotts.axera/releases/download/v1.0/encoder-en.onnx
+wget https://github.com/ml-inory/melotts.axera/releases/download/v1.0/decoder-zh.axmodel
+wget https://github.com/ml-inory/melotts.axera/releases/download/v1.0/decoder-jp.axmodel
+wget https://github.com/ml-inory/melotts.axera/releases/download/v1.0/decoder-en.axmodel
